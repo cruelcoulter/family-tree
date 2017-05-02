afef1dfc231d9b30b9eb4b9a0318ed347dbd2f95
Ron Coulter <ron@cruelcoulter.com>
2017-05-01 21:23:00 -0400

    added home page and header

---
+++ index.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+<link rel="stylesheet" href="https://unpkg.com/purecss@0.6.2/build/pure-min.css" integrity="sha384-UQiGfs9ICog+LwheBSRCt1o5cbyKIHbwjWscjemyBMT9YCUMZffs6UqUTd0hObXD" crossorigin="anonymous">
+<!--[if lte IE 8]>
+    <link rel="stylesheet" href="https://unpkg.com/purecss@0.6.2/build/grids-responsive-old-ie-min.css">
+<![endif]-->
+<!--[if gt IE 8]><!-->
+    <link rel="stylesheet" href="https://unpkg.com/purecss@0.6.2/build/grids-responsive-min.css">
+<!--<![endif]-->
+<title>Slovak/Perko/Kmetz/Serwatka family tree</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+	
+
+</body>
+</html>
